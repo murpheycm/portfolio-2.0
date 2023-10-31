61d66d7aa28d71177db3c414a41a5bad8e772663
murpheycm <124391666+murpheycm@users.noreply.github.com>
2023-10-31 12:15:38 -0400

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,79 @@
+import './App.css';
+import './index.css';
+import NavBar from '';
+import Header from '';
+import About from '';
+import Projects from '';
+import Contact from '';
+import Footer from '';
+import React, { useState, useEffect } from 'react';
+
+function App() {
+
+  // const [showScroll, setShowScroll] = useState(false);
+
+  // const checkScrollTop = () => {
+  //   if (!showScroll && window.pageYOffset > 400) {
+  //     setShowScroll(true);
+  //   } else if (showScroll && window.pageYOffset <= 400) {
+  //     setShowScroll(false);
+  //   }
+  // };
+
+  // const scrollTop = () => {
+  //   window.scrollTo({ top: 0, behavior: 'smooth' });
+  // };
+
+  // useEffect(() => {
+  //   const handleScroll = () => {
+  //     if (window.scrollY > 100) {
+  //       setShowScroll(true);
+  //     } else {
+  //       setShowScroll(false);
+  //     }
+  //   };
+
+  //   window.addEventListener('scroll', checkScrollTop);
+  //   window.addEventListener('scroll', handleScroll);
+
+  //   return () => {
+  //     window.removeEventListener('scroll', checkScrollTop);
+  //     window.removeEventListener('scroll', handleScroll);
+  //   };
+  // }, []);
+
+  return (
+    <>
+      <section id='home'>
+        <div className='w-100 h-100 bg-gradient-to-b from-indigo-950 to-violet-700'>
+          <div>
+            <NavBar />
+          </div>
+          <div>
+            <Header />
+          </div>
+          <div>
+            <About />
+          </div>
+          <div>
+            <Projects />
+          </div>
+          <div>
+            <Contact />
+          </div>
+          <div>
+            <Footer />
+          </div>
+    
+{/*           {showScroll && (
+            <div className="scrollTop" onClick={scrollTop}>
+              <i class="fa-solid fa-caret-up"></i>
+            </div>
+          )} */}
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default App;
