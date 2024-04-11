f3ad470e659424cde361e85f288285f924602cca
Cathryn Murphey <cmmurphey@gmail.com>
2024-04-11 11:53:29 -0700

    Update header image stretch

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ export default function Header() {
       <div className="flex">
           <div className="bg-gradient-to-b from-orange-200 to-rose-300 shadow-1xl dark:bg-neutral-600 md:max-w-xl">
             <img
-              className="h-50 w-full object-cover md:h-full md:w-48"
+              className="h-50 w-full object-fill md:h-full md:w-48"
               src="./images/MM.jpg"
               alt="" ></img>
           </div>
