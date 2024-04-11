6ce6ca208d60b53964d4caabe4df2707bbbc5864
Cathryn Murphey <cmmurphey@gmail.com>
2024-04-11 11:58:13 -0700

    Altering screen size

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 export default function Header() {
   return (
       <div className="flex">
-          <div className="bg-gradient-to-b from-orange-200 to-rose-300 shadow-1xl dark:bg-neutral-600 md:max-w-xl">
+          <div className="bg-gradient-to-b from-orange-200 to-rose-300 shadow-1xl dark:bg-neutral-600 max-w-screen">
             <img
-              className="h-50 w-full object-fill md:h-full md:w-48"
+              className="object-fill max-w-screen"
               src="./images/MM.jpg"
               alt="" ></img>
           </div>
