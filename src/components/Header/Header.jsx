de8131456f20282ea328c2130c1310b132cbbc4c
Cathryn Murphey <cmmurphey@gmail.com>
2024-04-16 22:03:02 -0400

    Update header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,13 +2,38 @@ import React from 'react';
 
 export default function Header() {
   return (
-      <div className="flex">
-          <div className="bg-gradient-to-b from-orange-200 to-rose-300 shadow-1xl dark:bg-neutral-600 max-w-screen">
+      <div className="flex flex-row justify-items-center">
+          {/* <div className="bg-gradient-to-b from-orange-200 to-rose-300 shadow-1xl dark:bg-neutral-600 max-w-screen">
             <img
               className="object-center max-w-screen"
               src="./images/MM.jpg"
               alt="" ></img>
+          </div> */}
+          <div>
+            <svg viewBox="0 0 75 75" width="200px">
+              <path d="m5,22 18,-18 28,0 18,18 0,28 -18,18, -28,0 -18,-18z" stroke="black" stroke-width="2" fill="white" />
+            </svg>
           </div>
-        </div>
+          <div>
+            <svg viewBox="0 0 75 75" width="200px">
+              <path d="m5,22 18,-18 28,0 18,18 0,28 -18,18, -28,0 -18,-18z" stroke="black" stroke-width="2" fill="white" />
+            </svg>
+          </div>
+          <div>
+            <svg viewBox="0 0 75 75" width="200px">
+              <path d="m5,22 18,-18 28,0 18,18 0,28 -18,18, -28,0 -18,-18z" stroke="black" stroke-width="2" fill="white" />
+            </svg>
+          </div>
+          <div>
+            <svg viewBox="0 0 75 75" width="200px">
+              <path d="m5,22 18,-18 28,0 18,18 0,28 -18,18, -28,0 -18,-18z" stroke="black" stroke-width="2" fill="white" />
+            </svg>
+          </div>
+          <div>
+            <svg viewBox="0 0 75 75" width="200px">
+              <path d="m5,22 18,-18 28,0 18,18 0,28 -18,18, -28,0 -18,-18z" stroke="black" stroke-width="2" fill="white" />
+            </svg>
+          </div>
+      </div>
   )
 };
