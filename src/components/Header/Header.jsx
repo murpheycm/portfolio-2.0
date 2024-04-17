5a4d9aabef5b3ea34f94a797e5e6e363f5a6deae
Cathryn Murphey <cmmurphey@gmail.com>
2024-04-16 22:20:53 -0400

    Update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Header() {
   return (
-      <div className="grid grid-cols-5 gap-2 content-evenly my-30">
+      <div className="h-50 grid grid-cols-5 gap-2 content-center my-10">
           {/* <div className="bg-gradient-to-b from-orange-200 to-rose-300 shadow-1xl dark:bg-neutral-600 max-w-screen">
             <img
               className="object-center max-w-screen"
