4d4da516e523437e80d88320a13d357faffc6b3a
Cathryn Murphey <cmmurphey@gmail.com>
2024-05-29 22:32:54 -0400

    Update speedgarage card

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -78,64 +78,68 @@ export default function Projects() {
                     <ul id="cards">
                         <li class="card" id="card-1">
                             <div class="card-content">
-                            <div>
-                                <h2>Musikero</h2>
-                                <p>A music app that allows users to search for artists of interest 
-                                and view similar artists, a sample playlist, and tour dates.</p>
-                            </div>
-                            <figure>
-                                <img src="./images/musikero.jpg" alt="card one - Musikero"></img>
-                            </figure>
+                                <div>
+                                    <h2>Musikero</h2>
+                                    <p>A music app that allows users to search for artists of interest 
+                                    and view similar artists, a sample playlist, and tour dates.</p>
+                                </div>
+                                <figure>
+                                    <img src="./images/musikero.jpg" alt="card one - Musikero"></img>
+                                </figure>
                             </div>
                         </li>
 
                         <li class="card" id="card-2">
                             <div class="card-content">
-                            <div>
-                                <h2>Lost Threads</h2>
-                                <p>A forum-based website catering to abandoned locations throughout Virginia.</p>
-                            </div>
-                            <figure>
-                                <img src="./images/lost-threads.jpg" alt="card two - lost threads"></img>
-                            </figure>
+                                <div>
+                                    <h2>Lost Threads</h2>
+                                    <p>A forum-based website catering to abandoned locations throughout Virginia.</p>
+                                </div>
+                                <figure>
+                                    <img src="./images/lost-threads.jpg" alt="card two - lost threads"></img>
+                                </figure>
                             </div>
                         </li>
 
                         <li class="card" id="card-3">
                             <div class="card-content">
-                            <div>
-                                <h2>NightCap</h2>
-                                <p> A react app for discovering mixed drinks in your area.</p>
-                            </div>
-                            <figure>
-                                <img src="./images/nightcap.JPG" alt="card three - nightcap"></img>
-                            </figure>
+                                <div>
+                                    <h2>NightCap</h2>
+                                    <p> A react app for discovering mixed drinks in your area.</p>
+                                </div>
+                                <figure>
+                                    <img src="./images/nightcap.JPG" alt="card three - nightcap"></img>
+                                </figure>
                             </div>
                         </li>
 
                         <li class="card" id="card-4">
                             <div class="card-content">
-                            <div>
-                                <h2>Book Search</h2>
-                                <p>This is the content of card four. Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                            </div>
-                            <figure>
-                                <img src="./images/MM.jpg" alt="card four - book search"></img>
-                            </figure>
+                                <div>
+                                    <h2>Book Search</h2>
+                                    <p>This is the content of card four. Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                                </div>
+                                <figure>
+                                    <img src="./images/MM.jpg" alt="card four - book search"></img>
+                                </figure>
                             </div>
                         </li>
 
                         <li class="card" id="card-5">
                             <div class="card-content">
-                            <div>
-                                <h2>SpeedGarage, LLC.</h2>
-                                <p>A simple landing page with the business description and contact information for a local auto mechanic.</p>
-                            </div>
-                            <figure>
-                                <img src="./images/speedgarage1.png" alt="card five - Speed Garage, LLC."></img>
-                                <img src="./images/speedgarage2.png" alt="card five - Speed Garage, LLC."></img>
-                                <img src="./images/speedgarage3.png" alt="card five - Speed Garage, LLC."></img>
-                            </figure>
+                                <div>
+                                    <h2>SpeedGarage, LLC.</h2>
+                                    <p>A simple landing page with the business description and contact information for a local auto mechanic.</p>
+                                </div>
+                                <figure>
+                                    <img src="./images/speedgarage1.png" alt="card five - Speed Garage, LLC."></img>
+                                </figure>
+                                <figure>
+                                    <img src="./images/speedgarage2.png" alt="card five - Speed Garage, LLC."></img>
+                                </figure>
+                                <figure>
+                                    <img src="./images/speedgarage3.png" alt="card five - Speed Garage, LLC."></img>
+                                </figure>
                             </div>
                         </li>
                     </ul>
